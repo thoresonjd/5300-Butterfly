ba9a2730283d18cbebfe48fb691258f39171873b
Bobby Brown <rbrown3@seattleu.edu>
2023-01-25 19:14:31 -0800

    Added test function, comments to heap_storage.cpp

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -1,10 +1,64 @@
-#include <cstring>
+/**
+ * @file        heap_storage.cpp for 5300-BUTTERFLY
+ *              This is the heap storage engine for a database management 
+ *              system.
+ * @authors     Bobby Brown rbrown3 & Denis Rajic drajic
+ * @date        Created 1/21/2023
+ * @version     Milestone 2
+ * 
+*/
 
+#include <cstring>
 #include "heap_storage.h"
-bool test_heap_storage() {return true;}
 
 typedef u_int16_t u16;
 
+/**
+ * Test heap storage function. Returns true if all tests pass.
+ * 
+ * Code provided from requirements.
+*/
+bool test_heap_storage() 
+{
+    ColumnNames column_names;
+	column_names.push_back("a");
+	column_names.push_back("b");
+	ColumnAttributes column_attributes;
+	ColumnAttribute ca(ColumnAttribute::INT);
+	column_attributes.push_back(ca);
+	ca.set_data_type(ColumnAttribute::TEXT);
+	column_attributes.push_back(ca);
+    HeapTable table1("_test_create_drop_cpp", column_names, column_attributes);
+    table1.create();
+    std::cout << "create ok" << std::endl;
+    // drop makes the object unusable because of BerkeleyDB restriction
+    table1.drop();  
+    std::cout << "drop ok" << std::endl;
+    
+    HeapTable table("_test_data_cpp", column_names, column_attributes);
+    table.create_if_not_exists();
+    std::cout << "create_if_not_exsts ok" << std::endl;
+
+    ValueDict row;
+    row["a"] = Value(12);
+    row["b"] = Value("Hello!");
+    std::cout << "try insert" << std::endl;
+    table.insert(&row);
+    std::cout << "insert ok" << std::endl;
+    Handles* handles = table.select();
+    std::cout << "select ok " << handles->size() << std::endl;
+    ValueDict *result = table.project((*handles)[0]);
+    std::cout << "project ok" << std::endl;
+    Value value = (*result)["a"];
+    if (value.n != 12)
+    	return false;
+    value = (*result)["b"];
+    if (value.s != "Hello!")
+		return false;
+    table.drop();
+
+    return true;
+}
 
 // ---- SlottedPage methods ---- // 
 
