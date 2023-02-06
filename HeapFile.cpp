5d0c9dc255e13a924cec3e96868cb2601201c50f
Justin Thoreson <thoresonjd@gmail.com>
2023-02-06 12:19:59 -0800

    Added HeapFile

---
+++ HeapFile.cpp
@@ -0,0 +1,90 @@
+/**
+ * @file HeapFile.cpp
+ * @author K Lundeen
+ * @see Seattle University, CPSC5300
+ */
+#include <cstring>
+#include "db_cxx.h"
+#include "HeapFile.h"
+
+using u16 = u_int16_t;
+using u32 = u_int32_t;
+
+HeapFile::HeapFile(string name) : DbFile(name), dbfilename(""), last(0), closed(true), db(_DB_ENV, 0) {
+    this->dbfilename = this->name + ".db";
+}
+
+void HeapFile::create(void) {
+    u32 flags = DB_CREATE | DB_EXCL;
+    this->db_open(flags);
+    SlottedPage* page = get_new(); // force one page to exist
+    delete page;
+}
+
+void HeapFile::drop(void) {
+    this->close();
+    Db db(_DB_ENV, 0);
+    this->db.remove(this->dbfilename.c_str(), nullptr, 0);
+}
+
+void HeapFile::open(void) {
+    this->db_open();
+}
+
+void HeapFile::close(void) {
+    this->db.close(0);
+    this->closed = true;
+}
+
+SlottedPage* HeapFile::get_new(void) {
+    char block[DbBlock::BLOCK_SZ];
+    std::memset(block, 0, sizeof(block));
+    Dbt data(block, sizeof(block));
+
+    int block_id = ++this->last;
+    Dbt key(&block_id, sizeof(block_id));
+
+    // write out an empty block and read it back in so Berkeley DB is managing the memory
+    SlottedPage* page = new SlottedPage(data, this->last, true);
+    this->db.put(nullptr, &key, &data, 0); // write it out with initialization done to it
+    delete page;
+    this->db.get(nullptr, &key, &data, 0);
+    return new SlottedPage(data, this->last);
+}
+
+SlottedPage *HeapFile::get(BlockID block_id) {
+    Dbt key(&block_id, sizeof(block_id)), data;
+    this->db.get(nullptr, &key, &data, 0);
+    return new SlottedPage(data, block_id, false);
+}
+
+void HeapFile::put(DbBlock* block) {
+    BlockID block_id = block->get_block_id();
+    Dbt key(&block_id, sizeof(block_id));
+    this->db.put(nullptr, &key, block->get_block(), 0);
+}
+
+BlockIDs* HeapFile::block_ids() const {
+    BlockIDs* block_ids = new BlockIDs();
+    for (BlockID block_id = 1; block_id <= this->last; block_id++)
+        block_ids->push_back(block_id);
+    return block_ids;
+}
+
+u32 HeapFile::get_block_count() {
+    DB_BTREE_STAT *stat;
+    this->db.stat(nullptr, &stat, DB_FAST_STAT);
+    uint32_t bt_ndata = stat->bt_ndata;
+    std::free(stat);
+    return bt_ndata;
+}
+
+void HeapFile::db_open(uint flags) {
+    if (!this->closed) return;
+    this->db.set_message_stream(_DB_ENV->get_message_stream());
+    this->db.set_error_stream(_DB_ENV->get_error_stream());
+    this->db.set_re_len(DbBlock::BLOCK_SZ); // record length - will be ignored if file already exists
+    this->db.open(nullptr, this->dbfilename.c_str(), nullptr, DB_RECNO, flags, 0644);
+    this->last = flags ? 0 : get_block_count();
+    this->closed = false;
+}
---
+++ HeapFile.h
@@ -0,0 +1,103 @@
+/*
+ * @file HeapFile.h - Implementation of storage_engine with a heap file structure.
+ * HeapFile: DbFile
+ *
+ * @author Kevin Lundeen
+ * @see "Seattle University, CPSC5300, Winter 2023"
+ */
+
+#pragma once
+
+#include "db_cxx.h"
+#include "SlottedPage.h"
+
+
+/**
+ * @class HeapFile - heap file implementation of DbFile
+ *
+ * Heap file organization. Built on top of Berkeley DB RecNo file. There is one
+ * of our database blocks for each Berkeley DB record in the RecNo file.
+ * In this way we are using Berkeley DB for buffer management and file
+ * management. Uses SlottedPage for storing records within blocks.
+ */
+class HeapFile : public DbFile {
+public:
+    /**
+     * Constructor
+     * @param name
+     */
+    HeapFile(std::string name) : DbFile(name), dbfilename(""), last(0), closed(true), db(_DB_ENV, 0) {}
+
+    virtual ~HeapFile() {}
+
+    HeapFile(const HeapFile& other) = delete;
+
+    HeapFile(HeapFile&& temp) = delete;
+
+    HeapFile& operator=(const HeapFile& other) = delete;
+
+    HeapFile& operator=(HeapFile&& temp) = delete;
+
+    /**
+     * Create physical database file
+     */
+    virtual void create(void);
+
+    /**
+     * Remove physical database file
+     */
+    virtual void drop(void);
+
+    /**
+     * Open the database file
+     */
+    virtual void open(void);
+
+    /**
+     * Close the database file
+     */
+    virtual void close(void);
+
+    /**
+     * Allocate a new block for the database file.
+     * Returns the new empty DbBlock that is managing the records in this block and its block id.
+     */
+    virtual SlottedPage* get_new(void);
+
+    /**
+     * Retrieves a block from the database file
+     * @param block_id The id of the block to retrieve
+     * @return A slotted page, the data of the block requested
+     */
+    virtual SlottedPage* get(BlockID block_id);
+
+    /**
+     * Writes a block to the database file
+     * @param block The block to write to the database file
+     */
+    virtual void put(DbBlock* block);
+
+    /**
+     *  Retrieves all block IDs of blocks within the database file
+     */
+    virtual BlockIDs* block_ids() const;
+
+    /**
+     * Retrieves the last block ID within the file
+     */
+    virtual u_int32_t get_last_block_id() { return last; }
+
+protected:
+    std::string dbfilename;
+    u_int32_t last;
+    bool closed;
+    Db db;
+
+    /**
+     * Open the Berkeley DB database file
+     * @param flags Flags to provide the Berkeley DB database file
+     */ 
+    virtual void db_open(uint flags = 0);
+
+    virtual uint32_t get_block_count();
+};
