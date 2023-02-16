13249afe5acc626fc81d365e085ebaf462c933f4
Justin Thoreson <thoresonjd@gmail.com>
2023-02-16 14:06:30 -0800

    Fixed bug in ParseTreeToString.cpp where DROP INDEX was not converted to a string properly

--- ParseTreeToString.cpp
+++ ParseTreeToString.cpp
@@ -249,6 +249,9 @@ string ParseTreeToString::drop(const DropStatement* stmt) {
         case DropStatement::kTable:
             ret += "TABLE ";
             break;
+        case DropStatement::kIndex:
+            ret += "INDEX " + string(stmt->indexName) + " FROM ";
+            break;
         default:
             ret += "? ";
     }
